5662371bdceea17503f8fb2ed57062e351811d5d
William Drozdek <syseng75@gmail.com>
2022-08-17 11:48:39 +0200

    entry commit, version empty

---
+++ src/main/java/com/it/impulseS/ImpulseSApplication.java
@@ -0,0 +1,13 @@
+package com.it.impulseS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpulseSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpulseSApplication.class, args);
+	}
+
+}
